4311238b08ce34655bf413bf70d49534039525bc
meg768 <meg768@users.noreply.github.com>
2017-11-13 14:42:41 +0100

    -

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var WifiConnection = require('./src/wifi-connection.js');
 var wifi = new WifiConnection();
 
-wifi.connectToNetwork({ssid:'Julia', psk:'potatismos'}).then(() => {
-    console.log('Connected to network.');
-})
-.catch((error) => {
-    console.log(error);
+wifi.getConnectionState().then((connected) => {
+    if (connected)
+        console.log('Connected to network.');
+    else
+        console.log('Not connected to network.');
 });
